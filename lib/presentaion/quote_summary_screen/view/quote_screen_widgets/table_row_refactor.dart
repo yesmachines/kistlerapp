8ced781c81d331662d85ba70b93a0654e71fd491
MeAnandCbgit config --global user.email anandcb55@gmail.comgit config --listgit config --globalgit config --global user.name MeAnandCb <anandcb55@gmail.com>
2023-08-22 22:53:01 +0530

    price quatation page updated

--- lib/presentaion/quote_summary_screen/view/quote_screen_widgets/table_row_refactor.dart
+++ lib/presentaion/quote_summary_screen/view/quote_screen_widgets/table_row_refactor.dart
@@ -1,22 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:kistler/core/constants.dart/color.dart';
 
-class TableRowRefactor extends StatelessWidget {
+class TableRowRefactor extends StatefulWidget {
   const TableRowRefactor({
     super.key,
   });
 
+  @override
+  State<TableRowRefactor> createState() => _TableRowRefactorState();
+}
+
+class _TableRowRefactorState extends State<TableRowRefactor> {
+  int selectedNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Table(
       border: TableBorder.all(color: ColorConstant.kistlerBrandGreen),
       defaultVerticalAlignment: TableCellVerticalAlignment.middle,
       columnWidths: {
-        0: FlexColumnWidth(1),
-        1: FlexColumnWidth(2.8),
-        2: FlexColumnWidth(1.5),
+        0: FlexColumnWidth(1.7),
+        1: FlexColumnWidth(2.9),
+        2: FlexColumnWidth(1.8),
+        3: FlexColumnWidth(2.5),
+        4: FlexColumnWidth(1.5),
       },
       children: [
+        TableRow(
+          children: [
+            TableCell(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                child: Text(
+                  'No',
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            TableCell(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                child: Text(
+                  'Name',
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            TableCell(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                child: Text(
+                  'Qty',
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            TableCell(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                child: Text(
+                  'Price',
+                  style: TextStyle(fontWeight: FontWeight.w900),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            TableCell(
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                child: Center(
+                  child: Text(
+                    'Dlt',
+                    style: TextStyle(fontWeight: FontWeight.w900),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
         TableRow(
           children: [
             TableCell(
@@ -34,18 +98,43 @@ class TableRowRefactor extends StatelessWidget {
                 child: Text(
                   'SCM300',
                   textAlign: TextAlign.start,
+                  maxLines: 5,
+                  overflow: TextOverflow.ellipsis,
                 ),
               ),
             ),
             TableCell(
               child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    height: 40,
+                    width: 40,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: ColorConstant.kistlerBrandGreen)),
+                      ),
+                    ),
+                  )),
+            ),
+            TableCell(
+              child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Text(
-                  '\$ 35600 ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
+                  '33 510',
                 ),
               ),
+            ),
+            TableCell(
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Icon(
+                    Icons.delete,
+                    size: 22,
+                  )),
             ),
           ],
         ),
@@ -55,7 +144,7 @@ class TableRowRefactor extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Text(
-                  '02',
+                  '01',
                   textAlign: TextAlign.center,
                 ),
               ),
@@ -64,56 +153,43 @@ class TableRowRefactor extends StatelessWidget {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Text(
-                  'Roller support frame to...',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
+                  'SCM300',
                   textAlign: TextAlign.start,
-                ),
-              ),
-            ),
-            TableCell(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Text(
-                  '\$ 1100 ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-          ],
-        ),
-        TableRow(
-          children: [
-            TableCell(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Text(
-                  '03',
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            TableCell(
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                child: Text(
-                  'Roller support frame to...',
-                  maxLines: 1,
+                  maxLines: 5,
                   overflow: TextOverflow.ellipsis,
-                  textAlign: TextAlign.start,
                 ),
               ),
             ),
             TableCell(
               child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    height: 40,
+                    width: 40,
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  )),
+            ),
+            TableCell(
+              child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 child: Text(
-                  '\$ 111 ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
+                  '33 510',
                 ),
               ),
+            ),
+            TableCell(
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Icon(
+                    Icons.delete,
+                    size: 22,
+                  )),
             ),
           ],
         ),
